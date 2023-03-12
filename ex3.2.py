54cf40f11c223646f7b3d7bbb5fe0ea5314f8580
Kenzy Hamed <104325633+Kenzyhamed@users.noreply.github.com>
2023-03-11 23:28:39 -0700

    Update ex3.2.py

--- ex3.2.py
+++ ex3.2.py
@@ -1,53 +1,59 @@
 import json
-import time
+import timeit
+from urllib.request import urlopen
 import matplotlib.pyplot as plt
+def binary_search( arr, x, pick_position):
+    low = 0
+    high = len(arr) - 1
+    position_picker_flag = False
+    while low <= high and x >= arr[low] and x <= arr[high]:
+        if low == high:
+            if arr[low] == x:
+                return low
+        return -1
+        if position_picker_flag == True:
+            pos = (low+hign) //2 
+        if position_picker_flag == False:
+            pos = pick_position
+            position_picker_flag = True
+        if arr[pos] == x:
+            return pos
+        if arr[pos] < x:
+            low=pos +1
+        else:
+            high=pos-1
+    return -1
+def main():
+    with urlopen('https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment3/ex2data.json') as inUrl:
+        arr_search = json.load(inUrl)
+    with urlopen('https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment3/ex2tasks.json') as inUrl:
+        arr_tasks = json.load(inUrl)
 
+    j=0
+    timef =[]
+    midpoints = arr_tasks
+    task_list= range(len(arr_tasks) )
 
-with open("ex2data.json") as f:
-    data = json.load(f)
+    while j != len(arr_tasks) :
+        timef.append(timeit.timeit(f'binary_search({arr_search},{arr_tasks[j]},{arr_tasks[j]})',setup='from __main__ import binary_search',number=100))
+        j+=1
+    #this timing graph slows down the program significantly
+    '''plt.subplot(1, 2, 1)
+    pit.scatter(task_list, timef, label='Time to find task 100 times')
+    plt.title('Time vs task number')
+    pit.xlabel ('Task Number')
+    pit.ylabel ('Time (s) ')
+    plt.legend()'''
+    #plt. subplot (1, 2,2) 
+    plt.scatter(task_list, midpoints, label='Chosen Midpoints') 
+    plt.legend()
+    plt.title( 'Chosen Midpoint vs task number')
+    plt.xlabel('Task Number')
+    plt.ylabel('Chosen Midpoint' )
     
-with open("ex2tasks.json") as f:
-    tasks = json.load(f)
+if __name__ == "__main__" :
+        main()
 
 
-def binary_search(arr, x, start, end, mid):
-    if start > end:
-        return False
-    
-    if arr[mid] == x:
-        return True
-    
-    if arr[mid] > x:
-        return binary_search(arr, x, start, mid-1, (start+mid-1)//2)
-    else:
-        return binary_search(arr, x, mid+1, end, (mid+1+end)//2)
-
-
-best_midpoints = []
-for task in tasks:
-    start = 0
-    end = len(data) - 1
-    best_time = float('inf')
-    best_midpoint = None
-    
-    for i in range(start, end+1):
-        mid = (start+i)//2
-        start_time = time.perf_counter()
-        found = binary_search(data, task, start, end, mid)
-        end_time = time.perf_counter()
-        elapsed_time = end_time - start_time
-        
-        if found and elapsed_time < best_time:
-            best_time = elapsed_time
-            best_midpoint = mid
-        
-    best_midpoints.append(best_midpoint)
-
-
-plt.scatter(tasks, best_midpoints)
-plt.title("Initial Midpoint vs. Performance")
-plt.xlabel("Search Task")
-plt.ylabel("Best Initial Midpoint")
-plt.show()
 
 
